f56fd5cf52baba1caf6208457369d8e1b068f9c5
tjwill95 <33507928+tjwill95@users.noreply.github.com>
2020-01-29 16:50:56 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -12,9 +12,12 @@ def main():
     scale = [1,1,1]
     try: os.mkdir(os.path.join(os.path.dirname(__file__),'Output')) #Creates an output folder if there isn't one yet
     except: pass
-    try:    FILE_NAME = u.FILE_NAME
+    try:    FILE_NAME = u.FILE_NAME #Checks to see if a file name has been set
     except: FILE_NAME = ""
+    try:    PRIMITIVE_TYPE = u.PRIMITIVE_TYPE #Checks to see if a primitive type has been set
+    except: PRIMITIVE_TYPE = ""
     if FILE_NAME != "":
+        #This section retrieves the input file and voxelizes the input STL
         shortName = FILE_NAME[:-4]
         filepath = os.path.join(os.path.dirname(__file__), 'Input',FILE_NAME)
         res = u.RESOLUTION-u.BUFFER*2
@@ -23,63 +26,69 @@ def main():
         scale[0] = objectBox[0]/(gridResX-u.BUFFER*2)
         scale[1] = max(objectBox[1:])/(gridResY-u.BUFFER*2)
         scale[2] = scale[1]
-    elif u.PRIMITIVE == True:
-        shortName = u.PRIMITIVE_TYPE
-        if u.PRIMITIVE_TYPE == "Heart":
+    elif PRIMITIVE_TYPE != "":
+        #This section generates the desired primitive
+        shortName = PRIMITIVE_TYPE
+        if PRIMITIVE_TYPE == "Heart":
             x0 = np.linspace(0.5,3.5,u.RESOLUTION)
             y0, z0 = x0, x0
             origShape = f.heart(x0,y0,z0,2,2,2)
         else:
             x0 = np.linspace(-50,50,u.RESOLUTION)
             y0, z0 = x0, x0
-            if u.PRIMITIVE_TYPE == "Cube":
+            if PRIMITIVE_TYPE == "Cube":
                 origShape = f.rect(x0,y0,z0,80,80,80)
-            elif u.PRIMITIVE_TYPE == "Silo":
+            elif PRIMITIVE_TYPE == "Silo":
                 origShape = f.union(f.sphere(x0,y0,z0,40),f.cylinderY(x0,y0,z0,-40,0,40))
-            elif u.PRIMITIVE_TYPE == "Cylinder":
+            elif PRIMITIVE_TYPE == "Cylinder":
                 origShape = f.cylinderX(x0,y0,z0,-40,40,40)
-            else: #Sphere
+            elif PRIMITIVE_TYPE == "Sphere":
                 origShape = f.sphere(x0,y0,z0,40)
+            else:
+                print("Given primitive type has not yet been implemented.")
     else:
-        print("Provide either a file name or a desired primative.")
+        print("Provide either a file name or a desired primitive.")
         return
     
+    #This section cleans up the input object
     rawObject = SDF3D(f.condense(origShape,2*u.BUFFER))
     i,j,k = rawObject.shape
-    
-    origShape = rawObject
     origShape = f.smooth(rawObject,iteration=1)
     print("Object Smoothed")
     if u.OBJ_MESH: generateMesh(origShape,scale,modelName=shortName+"Object")
     if u.PLOTTING: multiPlot(origShape,shortName+' Object',u.SAVE_PLOTS)
     
+    #This section finds the skeleton of the input object
     oSkele = skeleton(origShape,u.GTHRESH,u.STHRESH)
     oSkele = u.skeletonMods(oSkele)
     print("Skeleton Computed")
     if u.PLOTTING: multiPlot(oSkele,shortName+' Skeleton',u.SAVE_PLOTS)
     if u.SKELE_MESH: generateMesh(oSkele,scale,modelName=shortName+"Skeleton")
     
+    #If needed, this section refleshes the skeleton
     if u.COMPOUND_MODIFICATIONS or u.REFLESH_MESH:
         refleshed = f.smooth(reflesh(oSkele,iteration = u.RESOLUTION//8))
         print("Skeleton Refleshed")
         if u.REFLESH_MESH: generateMesh(refleshed,scale,modelName=shortName+"Refleshed")
         if u.COMPOUND_MODIFICATIONS: rawObject = refleshed
     
+    #If needed, this section finds the complemental skeleton
     if u.COMPLEMENTAL_SKELETON:
         flesh = SDF3D(f.subtract(oSkele,rawObject))
         flesh = f.smooth(flesh,iteration=1)
-        #if u.PLOTTING: multiPlot(flesh,shortName+' Flesh',u.SAVE_PLOTS)
         cSkele = skeleton(flesh,u.GTHRESH,u.STHRESH)
         cSkele = u.complementalSkeletonMods(cSkele)
         print("Complemental Skeleton Computed")
         if u.PLOTTING: multiPlot(cSkele,shortName+' Complemental Skeleton',u.SAVE_PLOTS)
         if u.COMP_SKELE_MESH: generateMesh(cSkele,scale,modelName=shortName+"Complemental Skeleton")
         
-        cRefleshed = reflesh(cSkele,iteration = u.RESOLUTION//8)
-        recreatedObject = f.smooth(f.union(f.thicken(SDF3D(oSkele),2.0),cRefleshed))
-        print("Complemental Skeleton Refleshed")
-        if u.PLOTTING: multiPlot(recreatedObject,shortName+' Remade',u.SAVE_PLOTS)
-        if u.COMP_REFLESH_MESH: generateMesh(recreatedObject,scale,modelName=shortName+"Remade")
+        #If needed, this section refleshes the complemental skeleton
+        if u.COMP_REFLESH_MESH:
+            cRefleshed = reflesh(cSkele,iteration = u.RESOLUTION//8)
+            recreatedObject = f.smooth(f.union(f.thicken(SDF3D(oSkele),2.0),cRefleshed))
+            print("Complemental Skeleton Refleshed")
+            if u.PLOTTING: multiPlot(recreatedObject,shortName+' Remade',u.SAVE_PLOTS)
+            generateMesh(recreatedObject,scale,modelName=shortName+"Remade")
     
 if __name__ == '__main__':
     main()
--- userInput.py
+++ userInput.py
@@ -1,33 +1,42 @@
 from skeleton import thickenSkeletonAdd, thickenSkeletonMult
 import Frep as f
 
-BUFFER = 10
-RESOLUTION = 150
+BUFFER = 10     #Number of empty layers around the object
+RESOLUTION = 150    #Maximum number of voxels in the X/Y directions
+#Gradient function sensitivity (Higher value = more gradient voxels)
 GTHRESH = 0.85
+#SDF Skeletal Sensitivity (Negative, higher magnitude = more skeletal voxels)
 STHRESH = -1
 
-PLOTTING = True
-SAVE_PLOTS = False
+PLOTTING = True     #Print plots as it goes through the skeletizing process
+SAVE_PLOTS = False  #Save the afore-printed plots to the Output folder
 
-OBJ_MESH = True
-SKELE_MESH = True
-REFLESH_MESH = True
+#Return the PLY of the original object after voxelizing and smoothing
+OBJ_MESH = True     
+SKELE_MESH = True   #Return the PLY of the skeleton
+REFLESH_MESH = True #Return the PLY of the object recreated from the skeleton
 
-COMPOUND_MODIFICATIONS = False
+#Recreate the object with changes to the skeleton and complemental skeleton
+COMPOUND_MODIFICATIONS = False  
 
-COMPLEMENTAL_SKELETON = True
-COMP_SKELE_MESH = True
+COMPLEMENTAL_SKELETON = True    #Find the Complemental Skeleton of the object
+COMP_SKELE_MESH = True          #Return the PLY of the Complemental Skeleton
+#Return the PLY of the object recreated from the complemental skeleton
 COMP_REFLESH_MESH = True
+
+#Put the name of the desired file below, or uncomment one of the example files
+#This file must be in the Input folder, set to be in the same directory as the
+#python files.
 
 #FILE_NAME = "Bird.stl"
 #FILE_NAME = "E.stl"
-#FILE_NAME = "3DBenchy_up.stl"
 #FILE_NAME = "3DBenchy.stl"
 #FILE_NAME = "bust_low.stl"
 #FILE_NAME = "wavySurface.stl"
-FILE_NAME = "hand_low.stl"
+#FILE_NAME = "hand_low.stl"
 
-PRIMITIVE = True   #Set to true if you would prefer a simple geometric object, then uncomment the one you want
+#If you would prefer a simple geometric object, uncomment the one you want and
+#make sure that all FILE_NAME options are commented out.
 PRIMITIVE_TYPE = "Heart"
 #PRIMITIVE_TYPE = "Cube"
 #PRIMITIVE_TYPE = "Sphere"
@@ -35,19 +44,30 @@ PRIMITIVE_TYPE = "Heart"
 #PRIMITIVE_TYPE = "Silo"
 
 def skeletonMods(u):
+    #This section allows you to make modifications to the skeleton of the 
+    #object. Reminder that internal voxels are negative, external voxels are 
+    #positive, and the magnitude of the voxel defines the size of the maximal
+    #sphere at that point
     i,j,k = u.shape
-    impact = -1
+    """
     ray = j-1
+    impact = -1
     while impact<0:
         if u[i//2-20,ray,100]<0:
             impact = 1
             u[i//2-20,ray,100]-=10
         else:
             ray -=1
+    """
     return u
 
 def complementalSkeletonMods(u):
+    #This section allows you to make modifications to the complemental skeleton
+    #of the object.  Reminder that internal voxels are negative, external 
+    #voxels are positive, and the magnitude of the voxel defines the size of 
+    #the maximal sphere at that point
     i,j,k = u.shape
+    """
     impact = -1
     ray = j-1
     while impact<0:
@@ -56,4 +76,5 @@ def complementalSkeletonMods(u):
             u[i//2-20,ray,100]-=10
         else:
             ray -=1
+    """
     return u
